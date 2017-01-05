6e1ab377c29afac02cff38f0dacc94839340d1b8
karanagarwal17 <karanagarwal1702@gmail.com>
2017-01-05 22:42:57 +0530

    added the header file

---
+++ struct.h
@@ -0,0 +1,11 @@
+typedef struct dev {
+  char fname[20];
+  char lname[20];
+  char field[40];
+  char location[40];
+  char email[40];
+  char position[40];
+  char language1[20];
+  char language2[20];
+  char language3[20];
+}dev;
