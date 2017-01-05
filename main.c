31ad1cd27bf98f43da838322c0de82759bfa7790
karanagarwal17 <karanagarwal1702@gmail.com>
2017-01-05 23:59:04 +0530

    major code refactor

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"struct.h"
+#include<stdlib.h>
+
+int main(){
+  int i=1;
+  while(1){
+    printf("Welcome to Developer Profile Management System\nChoose any of the following options\n1. Insert\n2. Display\n3. Query\n4. Exit\n");
+    scanf("%d",&i);
+    switch(i){
+      case 1 : insert();break;
+      case 2 : read();break;
+      case 3 : query();break;
+      case 4 : exit(0);break;
+    }
+  }
+  return 0;
+}
--- struct.h
+++ struct.h
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include<string.h>
+
 typedef struct dev {
   char fname[20];
   char lname[20];
@@ -7,3 +10,69 @@ typedef struct dev {
   char skill1[20];
   char skill2[20];
 }dev;
+
+void query();
+void read();
+void clear();
+void insert();
+
+
+
+void insert() {
+  dev d;
+  FILE *fp;
+  fp = fopen("data.dat","a");
+  printf("Enter First Name\n");
+  scanf("%s",d.fname);
+  printf("Enter Last Name\n");
+  scanf("%s",d.lname);
+  printf("Enter Location\n");
+  clear();
+  scanf("%[^\n]s",d.location);
+  printf("Enter Email\n");
+  scanf("%s",d.email);
+  printf("Enter Position\n");
+  clear();
+  scanf("%[^\n]s",d.position);
+  printf("Enter skill 1\n");
+  scanf("%s",d.skill1);
+  printf("Enter skill 2\n");
+  scanf("%s",d.skill2);
+  fwrite(&d,sizeof(dev),1,fp);
+  fclose(fp);
+}
+
+void clear(){
+  while ( getchar() != '\n' );
+}
+
+void read(){
+  int i=1;
+  FILE *fp;
+  dev d;
+  fp = fopen("data.dat","r");
+  printf("\n");
+  while(1){
+    i = fread(&d,sizeof(dev),1,fp);
+    if(i != 1) break;
+    printf("%-10s %-10s %-10s %-20s %-10s %-10s %-10s\n",d.fname, d.lname,d.location,d.email,d.position,d.skill1,d.skill2);
+  }
+  printf("\n");
+  fclose(fp);
+}
+
+void query(){
+  int i=1;
+  FILE *fp;
+  dev d;
+  fp = fopen("data.dat","r");
+  printf("\n");
+  while(1){
+    i = fread(&d,sizeof(dev),1,fp);
+    if(i != 1) break;
+    if(strcmp(d.fname,"karan") != 0) continue;
+    printf("%-10s %-10s %-20s %-10s %-10s %-10s %-10s\n",d.fname, d.lname,d.location,d.email,d.position,d.skill1,d.skill2);
+  }
+  printf("\n");
+  fclose(fp);
+}
